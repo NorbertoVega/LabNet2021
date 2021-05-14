259efbb54a5277c6ea60eb1a4836bace8ade46c4
norbert <norberto.a.vega@gmail.com>
2021-05-14 19:29:34 -0300

    Agrega controller, modelView  y vista para Regiones, los datos se muestran en una tabla

---
+++ Lab.Demo.EF/Lab.MVC/Controllers/RegionController.cs
@@ -0,0 +1,30 @@
+﻿using Lab.EF.Entities;
+using Lab.EF.Logic;
+using Lab.MVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Lab.MVC.Controllers
+{
+    public class RegionController : Controller
+    {
+        // GET: Region
+        public ActionResult Index()
+        {
+            var regionLogic = new RegionLogic();
+            List<Region> regiones = regionLogic.GetAll();
+
+            List<RegionView> regionsView = regiones.Select(r => new RegionView
+            {
+                Id = r.RegionID,
+                Description = r.RegionDescription
+
+            }).ToList(); 
+            
+            return View(regionsView);
+        }
+    }
+}
---
+++ Lab.Demo.EF/Lab.MVC/Models/RegionView.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Lab.MVC.Models
+{
+    public class RegionView
+    {
+        public int Id { get; set; }
+
+        public string Description { get; set; }
+    }
+}
