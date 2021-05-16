0bc45f950492099c5a826aa4fbc610855d65cafc
norbert <norberto.a.vega@gmail.com>
2021-05-16 03:12:42 -0300

    Agrega FetchAPIData y Photo(en la capa Data y Entities/API respectivamente) para consumir de la API de Nasa(con fotos que saca el Mars Rover). Crea PhotoView y PhotoController en MVC

---
+++ Lab.Demo.EF/Lab.EF.Data/API/FetchAPIData.cs
@@ -0,0 +1,33 @@
+﻿using Lab.EF.Entities.API;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab.EF.Data.API
+{
+    public class FetchAPIData
+    {
+        const string MARS_ROVERS_PHOTOS_URL = "https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&page=3&api_key=DEMO_KEY";
+        public List<Photo> Fetch()
+        {
+            try
+            {
+                var webClient = new WebClient();
+                
+                var datos = webClient.DownloadString(MARS_ROVERS_PHOTOS_URL);
+
+                var photoList = JsonConvert.DeserializeObject<List<Photo>>(datos);
+
+                return photoList;
+            }
+            catch
+            {
+                throw;
+            }
+        }
+    }
+}
---
+++ Lab.Demo.EF/Lab.MVC/Models/API/PhotoView.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Lab.MVC.Models.API
+{
+    public class PhotoView
+    {
+        public int Id { get; set; }
+        public string CameraName { get; set; }
+        public string ImgSource { get; set; }
+        public string EarthDate { get; set; }
+        public string RoverName { get; set; }
+    }
+}
