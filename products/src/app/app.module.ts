518b51cd8b1bd74e24ec3dffa1d91520f3708936
norbert <norberto.a.vega@gmail.com>
2021-05-19 00:48:50 -0300

    Agrega estructura básica(components,models y services) y agrega Angular Material

--- products/src/app/app.module.ts
+++ products/src/app/app.module.ts
@@ -3,14 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NewProductComponent } from './components/new-product/new-product.component';
+import { ProductsComponent } from './components/products/products.component';
+import { ListProductsComponent } from './components/list-products/list-products.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NewProductComponent,
+    ProductsComponent,
+    ListProductsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
